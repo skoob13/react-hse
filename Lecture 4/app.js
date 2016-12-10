a97cd3495d4a7c8d376be87dbc1216379a40e05f
skoob13 <skoob17@mail.ru>
2016-12-10 17:53:23 +0500

    добавлены overflow-y && max-height

--- Lecture 4/app.js
+++ Lecture 4/app.js
@@ -3,6 +3,9 @@ $("#send").click(function(event) {
   var title = $("#title-input").val();
   var text = $("#text-input").val();
 
+  $("#title-input").val();
+  $("#text-input").val();
+  
   if (title && text) {
     var container = $("#articles");
     container.prepend('<article class="blog-post"><h3>'+title+'</h3><p>'+text+'</p></article>');
