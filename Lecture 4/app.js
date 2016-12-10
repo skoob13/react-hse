e5a6ad9486cf58baa066a59137dc608ca520549f
skoob13 <skoob17@mail.ru>
2016-12-10 17:45:34 +0500

    Добавлены ивенты jquery

---
+++ Lecture 4/app.js
@@ -0,0 +1,10 @@
+$("#send").click(function(event) {
+  event.preventDefault();
+  var title = $("#title-input").val();
+  var text = $("#text-input").val();
+
+  if (title && text) {
+    var container = $("#articles");
+    container.prepend('<article class="blog-post"><h3>'+title+'</h3><p>'+text+'</p></article>');
+  }
+});
